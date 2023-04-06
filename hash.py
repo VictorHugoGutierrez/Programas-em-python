dce8582c868dca9cd4927d1987110466c7b30f80
Victor Hugo Gutierrez <105084189+VictorHugoGutierrez@users.noreply.github.com>
2023-04-05 22:56:35 -0300

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-
 def displayhash(hash):
   os.system('cls')
   print("TIC-TAC-TOE:")
@@ -13,7 +12,7 @@ def displayhash(hash):
     print()
 
 
-def verification(hash, symbol):
+def verificationWinner(hash, symbol):
   if hash[0][0] == symbol and hash[1][0] == symbol and hash[2][0] == symbol:
     print("You are Winner!")
     question()
@@ -32,7 +31,24 @@ def verification(hash, symbol):
   elif hash[0][2] == symbol and hash[1][1] == symbol and hash[2][0] == symbol:
     print("You are Winner!")
     question()
-
+  elif hash[0][1] == symbol and hash[1][1] == symbol and hash[2][1] == symbol:
+    print("You are Winner!")
+    question()
+  elif hash[1][0] == symbol and hash[1][1] == symbol and hash[1][2] == symbol:
+    print("You are Winner!")
+    question()
+    
+def verificationCases(hash):
+  verification = 0
+  for line in hash:
+    for word in line:
+      if word == 'X' or word == 'O':
+        verification = verification + 1
+      else:
+        break
+  if verification == 9:
+    print("Game Over")
+    question()
 
 def menuHash(hash):
   symbol = "O"
@@ -47,7 +63,8 @@ def menuHash(hash):
         symbol = "X"
         hash[0][0] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 2:
       if symbol == "X":
@@ -57,7 +74,8 @@ def menuHash(hash):
         symbol = "X"
         hash[0][1] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 3:
       if symbol == "X":
@@ -67,7 +85,8 @@ def menuHash(hash):
         symbol = "X"
         hash[0][2] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 4:
       if symbol == "X":
@@ -77,7 +96,8 @@ def menuHash(hash):
         symbol = "X"
         hash[1][0] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 5:
       if symbol == "X":
@@ -87,7 +107,8 @@ def menuHash(hash):
         symbol = "X"
         hash[1][1] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 6:
       if symbol == "X":
@@ -97,7 +118,8 @@ def menuHash(hash):
         symbol = "X"
         hash[1][2] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 7:
       if symbol == "X":
@@ -107,7 +129,8 @@ def menuHash(hash):
         symbol = "X"
         hash[2][0] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 8:
       if symbol == "X":
@@ -117,7 +140,8 @@ def menuHash(hash):
         symbol = "X"
         hash[2][1] = symbol
       displayhash(hash)
-      verification(hash, symbol)
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
       
     elif option == 9:
       if symbol == "X":
@@ -127,8 +151,8 @@ def menuHash(hash):
         symbol = "X"
         hash[2][2] = symbol
       displayhash(hash)
-      verification(hash, symbol)
-
+      verificationWinner(hash, symbol)
+      verificationCases(hash)
 
 def question():
   option = input("would you like to play again?" + " Yes or Not? ").upper()
@@ -139,11 +163,9 @@ def question():
     os.system('cls')
     sys.exit()
 
-
 def main():
   hash = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
   displayhash(hash)
   menuHash(hash)
-
 
 main()
